c0586661808e8f1d6abe2eda0995ac7bdfe37b05
devadath-k <devadathdathu7@gmail.com>
2024-08-20 17:34:59 +0530

    mongodb added

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const express=require('express');
 const app=express()
 const http=require('http').Server(app);
 const path=require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
+
+const userSchema = new mongoose.Schema({
+    socketId: String,
+    name: String
+});
+
+const User = mongoose.model('User', userSchema);
 
 http.listen(8080, function(){
     console.log('server ready on 8080');
@@ -22,18 +41,29 @@ const io=require('socket.io')(http);
 
 const users={};
 
-io.on('connection', socket =>{
-    socket.on('new-user-joined', name =>{
-        //console.log(`New user joined: ${name}`);
-        users[socket.id]=name;
+io.on('connection', socket => {
+    socket.on('new-user-joined', async (name) => {
+        // Add user to the database
+        const newUser = new User({ socketId: socket.id, name: name });
+        await newUser.save();
+
         socket.broadcast.emit('user-joined', name);
     });
-    socket.on('send', message=>{
-        socket.broadcast.emit('receive',{ message: message, name: users[socket.id]});
+
+    socket.on('send', async (message) => {
+        // Retrieve the user's name from the database
+        const user = await User.findOne({ socketId: socket.id });
+        socket.broadcast.emit('receive', { message: message, name: user.name });
     });
 
-    socket.on('disconnect', ()=>{
-        socket.broadcast.emit('leave', users[socket.id]);
-        delete users[socket.id];
-    })
-})
+    socket.on('disconnect', async () => {
+        // Retrieve the user's name from the database
+        const user = await User.findOne({ socketId: socket.id });
+        if (user) {
+            socket.broadcast.emit('leave', user.name);
+
+            // Remove user from the database
+            await User.deleteOne({ socketId: socket.id });
+        }
+    });
+});
